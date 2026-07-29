5f2b057aab7c9b2ff902f97df93df4ac0d3bd206
richie-rich90454 <rjiang880@gmail.com>
2026-07-29 09:11:00 +0800

    Convert Chat to TSX thin component using useChat hook

---
+++ frontend-modern/src/pages/Chat.tsx
@@ -0,0 +1,18 @@
+import { useChat } from '../hooks/useChat';
+import { MessageList } from '../components/MessageList';
+import { InputArea } from '../components/InputArea';
+
+export function Chat() {
+    const { messages, isLoading, sendMessage } = useChat();
+
+    return (
+        <div class="chat-view">
+            <MessageList messages={messages} isLoading={isLoading} />
+            <div class="chat-input-bar">
+                <div class="chat-input-bar-inner">
+                    <InputArea onSend={sendMessage} isLoading={isLoading} />
+                </div>
+            </div>
+        </div>
+    );
+}
