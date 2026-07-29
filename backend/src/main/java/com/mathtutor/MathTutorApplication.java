b4acbb1a09f18aba38924af33e1edf125df15d62
richie-rich90454 <rjiang880@gmail.com>
2026-07-29 08:44:41 +0800

    Add MathTutorApplication main class with Spring Boot entry point

---
+++ backend/src/main/java/com/mathtutor/MathTutorApplication.java
@@ -0,0 +1,12 @@
+package com.mathtutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathTutorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MathTutorApplication.class, args);
+    }
+}
