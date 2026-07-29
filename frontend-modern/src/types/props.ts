07a521cb7c9622a6f309ca41a4142c4a797df24d
richie-rich90454 <rjiang880@gmail.com>
2026-07-29 09:08:44 +0800

    Add props interfaces for Home, Chat, MessageList, InputArea

---
+++ frontend-modern/src/types/props.ts
@@ -0,0 +1,19 @@
+import type { Message } from './message';
+
+export interface AppProps {}
+
+export interface HomeProps {
+    onStart: () => void;
+}
+
+export interface ChatProps {}
+
+export interface MessageListProps {
+    messages: Message[];
+    isLoading: boolean;
+}
+
+export interface InputAreaProps {
+    onSend: (text: string) => void;
+    isLoading: boolean;
+}
