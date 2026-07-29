75748ceaeb1405a4931266893fa82f1e419bbb3a
richie-rich90454 <rjiang880@gmail.com>
2026-07-29 08:49:39 +0800

    Add ChatException for chat-related error handling

---
+++ backend/src/main/java/com/mathtutor/exception/ChatException.java
@@ -0,0 +1,12 @@
+package com.mathtutor.exception;
+
+public class ChatException extends RuntimeException {
+
+    public ChatException(String message) {
+        super(message);
+    }
+
+    public ChatException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
