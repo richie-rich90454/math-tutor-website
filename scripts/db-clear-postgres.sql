3217e4ef5cf2565502c3bc27ea4d02f650b7087d
richie-rich90454 <rjiang880@gmail.com>
2026-07-31 09:55:41 +0800

    Remove backend db scripts and backend runner

--- scripts/db-clear-postgres.sql
+++
@@ -1,8 +0,0 @@
--- PostgreSQL clear script (USE WITH CAUTION: destroys all data)
--- Marked as undesired for normal use. Only for development reset.
-DROP TABLE IF EXISTS usage_logs CASCADE;
-DROP TABLE IF EXISTS user_preferences CASCADE;
-DROP TABLE IF EXISTS messages CASCADE;
-DROP TABLE IF EXISTS chat_sessions CASCADE;
-DROP TABLE IF EXISTS sessions CASCADE;
-DROP TABLE IF EXISTS users CASCADE;
--- scripts/db-clear-sqlite.sql
+++
@@ -1,8 +0,0 @@
--- SQLite clear script (USE WITH CAUTION: destroys all data)
--- Marked as undesired for normal use. Only for development reset.
-DROP TABLE IF EXISTS usage_logs;
-DROP TABLE IF EXISTS user_preferences;
-DROP TABLE IF EXISTS messages;
-DROP TABLE IF EXISTS chat_sessions;
-DROP TABLE IF EXISTS sessions;
-DROP TABLE IF EXISTS users;
