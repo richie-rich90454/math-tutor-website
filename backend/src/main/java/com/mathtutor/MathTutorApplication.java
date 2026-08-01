b376c71141f86fb831e5442db7cc6c99f3c2c50a
richie-rich90454 <rjiang880@gmail.com>
2026-08-01 08:44:24 +0800

    Add Spring Boot application entry point for the backend

---
+++ backend/src/main/java/com/mathtutor/MathTutorApplication.java
@@ -0,0 +1,14 @@
+package com.mathtutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+public class MathTutorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MathTutorApplication.class, args);
+    }
+}
