c2591aedd497b0adf2eae97145751a84e3d5d614
richie-rich90454 <rjiang880@gmail.com>
2026-08-01 08:57:20 +0800

    Use Jackson 3 JsonNode package in JWT utility

--- backend/src/main/java/com/mathtutor/security/JwtUtil.java
+++ backend/src/main/java/com/mathtutor/security/JwtUtil.java
@@ -1,7 +1,7 @@
 package com.mathtutor.security;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ObjectMapper;
 import com.mathtutor.config.AppProperties;
 import org.springframework.stereotype.Component;
 
