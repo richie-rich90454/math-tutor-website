ae2399e557ea4eedb8bb2b4d74237ebf45432218
richie-rich90454 <rjiang880@gmail.com>
2026-08-01 14:42:24 +0800

    Load dotenv before Spring context startup

--- backend/src/main/java/com/mathtutor/MathTutorApplication.java
+++ backend/src/main/java/com/mathtutor/MathTutorApplication.java
@@ -1,5 +1,6 @@
 package com.mathtutor;
 
+import com.mathtutor.config.DotEnvLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
@@ -9,6 +10,7 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 public class MathTutorApplication {
 
     public static void main(String[] args) {
+        DotEnvLoader.load();
         SpringApplication.run(MathTutorApplication.class, args);
     }
 }
