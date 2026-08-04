61d28d83768ca107623cf522cf4daf4c96d32650
richie-rich90454 <rjiang880@gmail.com>
2026-08-04 18:48:36 +0800

    Legacy: regenerate hmn bundle

--- frontend-legacy/js/i18n/hmn.js
+++ frontend-legacy/js/i18n/hmn.js
@@ -245,4 +245,10 @@ TRANSLATION_TABLES["hmn"] = {
     sheetsLoadError: "Nqa cov nplooj ntawv tsis tau. Thov sim dua.",
     chatAskedBefore: "Yug lawm nug",
     chatFreshAnswer: "Tau cov lus teb tshiab",
+    settingsResumeLastChat: "Txuas ntxiv qhov kawg",
+    continueLearning: "Txuas ntxiv kawm",
+    recentTopics: "Tej ntsiab tsis ntev",
+    followUpExplain: "Piav yooj yim dua",
+    followUpExamples: "Ntxiv cov piv txwv",
+    followUpAnother: "Ib qho ntxiv",
 };
