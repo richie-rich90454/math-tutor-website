20b17d22a74d42dcd805a5f54963373d64c3b4ff
richie-rich90454 <rjiang880@gmail.com>
2026-08-04 18:48:38 +0800

    Legacy: regenerate ru bundle

--- frontend-legacy/js/i18n/ru.js
+++ frontend-legacy/js/i18n/ru.js
@@ -245,4 +245,10 @@ TRANSLATION_TABLES["ru"] = {
     sheetsLoadError: "Не удалось загрузить таблицы. Попробуйте ещё раз.",
     chatAskedBefore: "Спрашивалось ранее",
     chatFreshAnswer: "Получить новый ответ",
+    settingsResumeLastChat: "Продолжить последний чат",
+    continueLearning: "Продолжить обучение",
+    recentTopics: "Последние темы",
+    followUpExplain: "Объясни проще",
+    followUpExamples: "Больше примеров",
+    followUpAnother: "Ещё одну задачу",
 };
