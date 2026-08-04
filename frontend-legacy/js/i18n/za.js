78273bd75e7abf7a61543b58979f864545668988
richie-rich90454 <rjiang880@gmail.com>
2026-08-04 18:37:35 +0800

    Legacy: regenerate za translation bundle

--- frontend-legacy/js/i18n/za.js
+++ frontend-legacy/js/i18n/za.js
@@ -244,4 +244,6 @@ TRANSLATION_TABLES["za"] = {
     topicStatistics: "Doengjgaiq",
     gradeAll: "Gyoengq",
     sheetsLoadError: "Mbouj ndaej dawz bawj cingj. Cawz caiq demq.",
+    chatAskedBefore: "Daeq gvaq",
+    chatFreshAnswer: "Aeu daejmoeg moq",
 };
