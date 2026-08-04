a55ec4303b540d353ced414a7b25627aeb76687b
richie-rich90454 <rjiang880@gmail.com>
2026-08-04 18:37:29 +0800

    Legacy: regenerate i18n with asked-before/fresh-answer keys

--- frontend-legacy/js/i18n.js
+++ frontend-legacy/js/i18n.js
@@ -282,6 +282,8 @@ TRANSLATION_TABLES["en"] = {
     topicStatistics: "Statistics",
     gradeAll: "All",
     sheetsLoadError: "Could not load sheets. Please try again.",
+    chatAskedBefore: "Asked before",
+    chatFreshAnswer: "Get fresh answer",
 };
 (function () {
     var lang = null;
