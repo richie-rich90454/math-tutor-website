687c6b4684799545045a3cceefbc6c46fb91d7af
richie-rich90454 <rjiang880@gmail.com>
2026-08-04 18:48:35 +0800

    Legacy: regenerate dng bundle

--- frontend-legacy/js/i18n/dng.js
+++ frontend-legacy/js/i18n/dng.js
@@ -245,4 +245,10 @@ TRANSLATION_TABLES["dng"] = {
     sheetsLoadError: "Lianx dah laoh bav. Weex meik luh.",
     chatAskedBefore: "之前问过",
     chatFreshAnswer: "获取新答案",
+    settingsResumeLastChat: "恢复上次对话",
+    continueLearning: "继续学习",
+    recentTopics: "最近主题",
+    followUpExplain: "解释得更简单",
+    followUpExamples: "更多例子",
+    followUpAnother: "再来一题",
 };
