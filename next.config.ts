f992d66adecde21e73059e01d07681ac87694e19
richie-rich90454 <rjiang880@gmail.com>
2026-08-05 17:54:10 +0800

    Frontend: enable React Compiler (Next 16)

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,7 @@ const nextConfig: NextConfig = {
     compress: true,
     productionBrowserSourceMaps: false,
     poweredByHeader: false,
+    reactCompiler: true,
     onDemandEntries: {
         maxInactiveAge: 60 * 1000,
         pagesBufferLength: 2,
