0f901c0b67967f80a052e2916493b3620512a1a3
richie-rich90454 <rjiang880@gmail.com>
2026-08-05 16:34:44 +0800

    Frontend: drop unused ConceptProvider from every page load

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,6 @@ import {
 import "./globals.css";
 import { LanguageProvider } from "@/contexts/LanguageContext";
 import { ChatProvider } from "@/contexts/ChatContext";
-import { ConceptProvider } from "@/contexts/ConceptContext";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { ToastProvider } from "@/contexts/ToastContext";
 import SkipLink from "@/components/ui/SkipLink";
@@ -473,9 +472,7 @@ export default function RootLayout({
                         <AuthProvider>
                             <HtmlAttributes />
                             <SkipLink />
-                            <ConceptProvider>
-                                <ChatProvider>{children}</ChatProvider>
-                            </ConceptProvider>
+                            <ChatProvider>{children}</ChatProvider>
                         </AuthProvider>
                     </ToastProvider>
                 </LanguageProvider>
