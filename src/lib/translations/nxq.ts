ed5175d327083efbff32ec0abb85b8b1f4bae49f
richie-rich90454 <rjiang880@gmail.com>
2026-08-05 17:58:24 +0800

    i18n: add browserOutdated key (nxq)

--- src/lib/translations/nxq.ts
+++ src/lib/translations/nxq.ts
@@ -147,6 +147,7 @@ const nxq: Translations = {
     settingsLanguageSaved: "Seiq gge saiq ceel sai",
     settingsVersion: "Hhee laq lv sheeq mee AI v1.0.0",
     legacyVersion: "旧版",
+    browserOutdated: "您的浏览器可能无法支持全部功能",
     modernVersion: "新版",
     progressTitle: "Ngyu gge hheeq bbei hheeq",
     progressConversations: "Zal ddee chee la",
