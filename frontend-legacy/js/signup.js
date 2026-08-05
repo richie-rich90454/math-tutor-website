d0344fd2239e98f4d310a5dd35ed63e6b3091021
richie-rich90454 <rjiang880@gmail.com>
2026-08-05 15:56:41 +0800

    Legacy: IE6-safe text writes on signup page

--- frontend-legacy/js/signup.js
+++ frontend-legacy/js/signup.js
@@ -77,7 +77,7 @@
 
     function showError(msg) {
         var box = el("errorBox");
-        box.textContent = msg;
+        MathTutor.setText(box, msg);
         box.className = "auth-msg";
     }
 
