1c26f09a0aebb2f28d3616692095a79fa708aff5
richie-rich90454 <rjiang880@gmail.com>
2026-08-05 17:58:23 +0800

    i18n: add browserOutdated key (lic)

--- src/lib/translations/lic.ts
+++ src/lib/translations/lic.ts
@@ -146,6 +146,7 @@ const lic: Translations = {
     settingsLanguageSaved: "Lai kaeu hlaeu",
     settingsVersion: "Kafux Tomhau AI v1.0.0",
     legacyVersion: "旧版",
+    browserOutdated: "您的浏览器可能无法支持全部功能",
     modernVersion: "新版",
     progressTitle: "Meuz Tae Gaenx",
     progressConversations: "Hum",
